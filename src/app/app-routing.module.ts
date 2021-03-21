8c8cca09caa1ba2c5b79e2284ecd4924997c9c5f
zuratama <tuandt66742@gmail.com>
2021-03-21 20:37:38 +0700

    chore: Update cli config, add routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+
+const routes: Routes = [
+  {
+    path: "pages/demo",
+    loadChildren: () =>
+      import("./pages/demo/demo.module").then((m) => m.DemoModule),
+  },
+];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
