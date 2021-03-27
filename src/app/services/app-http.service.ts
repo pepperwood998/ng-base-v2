ffab2eb52b441803f5cab39bd59d1f757fd3c979
zuratama <tuandt66742@gmail.com>
2021-03-27 21:58:37 +0700

    chore: Add basic app http service.

---
+++ src/app/services/app-http.service.ts
@@ -0,0 +1,59 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AppHttpService {
+  constructor(private httpClient: HttpClient) {}
+
+  post<T>(uri: string, options?: HttpOptions) {
+    return this.request<T>(HttpMethod.POST, uri, options);
+  }
+
+  get<T>(uri: string, options?: HttpOptions) {
+    return this.request<T>(HttpMethod.GET, uri, options);
+  }
+
+  put<T>(uri: string, options?: HttpOptions) {
+    return this.request<T>(HttpMethod.PUT, uri, options);
+  }
+
+  delete<T>(uri: string, options?: HttpOptions) {
+    return this.request<T>(HttpMethod.DELETE, uri, options);
+  }
+
+  request<T>(
+    method: HttpMethod,
+    uri: string,
+    options?: HttpOptions,
+  ): Observable<T> {
+    const url = this.resolveUrl(uri);
+    return this.httpClient.request<T>(method.toString(), url, options);
+  }
+
+  private resolveUrl(uri: string): string {
+    let fullUrl = "";
+    if (/^(https|http):\/\/.*$/.test(uri)) {
+      fullUrl = uri;
+    } else {
+      fullUrl = `${environment.apiBaseUrl}${uri}`;
+    }
+    return fullUrl;
+  }
+}
+
+interface HttpOptions {
+  body?: any;
+  headers?: HttpHeaders;
+  params?: { [param: string]: string };
+}
+
+enum HttpMethod {
+  POST = "POST",
+  GET = "GET",
+  PUT = "PUT",
+  DELETE = "DELETE",
+}
